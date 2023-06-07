7511a68353f4b6f25cb161a6942d9b01cd78b481
Juan Gavira <juan.gavira@e-zigurat.com>
2023-06-07 13:59:01 +0200

    Fixing error routers in progress

--- www/config/router.php
+++ www/config/router.php
@@ -18,11 +18,20 @@ class Router{
         $url = explode('/', URL);
         
         /* FIXME: IT DOESNT WORK */
+
         $this->controller = !empty($url[1]) ? ucfirst($url[1]) : "Page";
         $this->method = !empty($url[2]) ? $url[2] : "_404";
+
         /* ---------------------- */
-        
+
         $this->controller = $this->controller . "Controller";
+
+        if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/controllers/' . $this->controller . '.php')){
+            
+        } else {
+            echo "no existe";
+        }
+
 
         require_once $_SERVER['DOCUMENT_ROOT'] . '/controllers/'. $this->controller . '.php';
     }
