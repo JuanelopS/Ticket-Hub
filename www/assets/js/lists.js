7e1efe206ce028a843d7426414ba9562e0614059
Juan Gavira <contacto@juangavira.me>
2023-06-15 09:26:14 +0200

    starting js eventlisteners

---
+++ www/assets/js/lists.js
@@ -0,0 +1,11 @@
+
+
+let btnDelete = document.querySelector('.btn_delete');
+let btnUpdate = document.querySelector('.btn_update');
+
+
+id.addEventListener('click', () => {
+
+    
+
+});
--- www/views/user/user.php
+++ www/views/user/user.php
@@ -3,7 +3,7 @@
     <div class="column">
         <table>
             <thead>
-                <tr style='font-weight: bold'>
+                <tr>
                     <th></th>
                     <th>id</th>
                     <th>email</th>
@@ -16,7 +16,7 @@
             foreach ($data as $value) {
                 $date = date_format(date_create($value['register_date']), 'd/m/Y H:i:s');
                 echo "<tr>";
-                echo "<td><i data-feather='x-octagon'></i><i data-feather='edit'></i></td>";
+                echo "<td><i class='btn_delete " . $value['id_user'] . "' data-feather='x-octagon'></i><i class='btn_update " . $value['id_user'] . "' data-feather='edit'></i></td>";
                 echo "<td>" . $value['id_user'] . "</td>";
                 echo "<td>" . $value['email'] . "</td>";
                 echo "<td>" . $value['name'] . "</td>";
