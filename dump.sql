a2c6b98b2eb63756bd1653eb86a2402c949d8267
Juan Gavira <contacto@juangavira.me>
2023-06-24 17:45:54 +0200

    updated db backup

--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Jun 23, 2023 at 05:17 PM
+-- Generation Time: Jun 24, 2023 at 03:45 PM
 -- Server version: 8.0.33
 -- PHP Version: 8.1.15
 
@@ -46,9 +46,11 @@ CREATE TABLE `tickets` (
 INSERT INTO `tickets` (`id`, `type`, `subject`, `ticket_text`, `state`, `creation_date`, `modification_date`, `user_id`, `priority`) VALUES
 (1, 1, 'test subject', 'test message', 1, '2023-06-22 12:48:16', '2023-06-22 12:48:16', 6, 1),
 (2, 2, 'No funciona internet', 'Pues eso, que no funciona internet, nada.', 1, '2023-06-23 12:29:17', '2023-06-23 12:29:17', 8, 3),
-(3, 3, 'No funciona la impresora', 'No impresora no funciona no hace nada', 1, '2023-06-23 13:14:53', '2023-06-23 13:14:53', 8, 2),
+(3, 3, 'No funciona la impresora', 'No impresora no funciona no hace nada', 1, '2023-06-23 13:14:53', '2023-06-24 15:38:38', 8, 2),
 (4, 4, 'no recibo emails', 'eso', 1, '2023-06-23 13:22:56', '2023-06-23 13:22:56', 8, 4),
-(5, 5, 'asdfasdfasdf', 'asdfasdfasdfasdfasdfasdf', 1, '2023-06-23 13:26:34', '2023-06-23 13:26:34', 8, 2);
+(5, 5, 'asdfasdfasdf', 'asdfasdfasdfasdfasdfasdf', 1, '2023-06-23 13:26:34', '2023-06-24 15:41:13', 8, 2),
+(6, 5, 'No funciona el microfono', 'No se me escucha', 1, '2023-06-24 06:40:38', '2023-06-24 06:40:38', 8, 2),
+(7, 6, 'no abre office', 'word no funciona, no se abre o tarda muchísimo en abrir', 1, '2023-06-24 06:59:55', '2023-06-24 06:59:55', 8, 2);
 
 -- --------------------------------------------------------
 
@@ -70,6 +72,51 @@ INSERT INTO `tickets_priority` (`id`, `priority_name`) VALUES
 (2, 'Low'),
 (3, 'High'),
 (4, 'Urgent');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets_responses`
+--
+
+CREATE TABLE `tickets_responses` (
+  `id` int NOT NULL,
+  `message` text COLLATE utf32_unicode_ci NOT NULL,
+  `message_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ticket_id` int NOT NULL,
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+--
+-- Dumping data for table `tickets_responses`
+--
+
+INSERT INTO `tickets_responses` (`id`, `message`, `message_date`, `ticket_id`, `user_id`) VALUES
+(15, 'asdfasdfasdf', '2023-06-24 09:55:42', 1, 7),
+(16, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:55:48', 1, 7),
+(17, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:55:50', 1, 7),
+(18, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:55:59', 1, 7),
+(19, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:55:59', 1, 7),
+(20, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:55:59', 1, 7),
+(21, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:56:00', 1, 7),
+(22, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:56:00', 1, 7),
+(23, 'sadfasdfasdfasdfasdfasdf', '2023-06-24 09:56:00', 1, 7),
+(24, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', '2023-06-24 09:56:08', 1, 7),
+(25, 'asdfaasdfasdfasdfasdf', '2023-06-24 09:56:29', 1, 7),
+(26, 'sadfasdfasdf', '2023-06-24 09:56:39', 2, 7),
+(27, 'asdfasdfasdf', '2023-06-24 09:56:42', 2, 7),
+(28, 'asfdasdfasdfasfasdf', '2023-06-24 09:56:46', 2, 7),
+(29, 'asdfasdfasdf', '2023-06-24 09:57:01', 2, 7),
+(30, 'asdfasdfasdf', '2023-06-24 09:58:50', 2, 8),
+(31, 'sadfasdfasdfasdfasdf', '2023-06-24 14:53:33', 2, 7),
+(32, 'cartucho cambiado', '2023-06-24 15:32:48', 3, 7),
+(33, 'test', '2023-06-24 15:33:50', 3, 7),
+(34, 'asdfasdfasdfasdf', '2023-06-24 15:34:37', 3, 7),
+(35, 'asdfasdfasdf', '2023-06-24 15:34:48', 3, 7),
+(36, 'asdfasdfasdfasdf', '2023-06-24 15:37:35', 3, 7),
+(37, 'asdfasdfasdfasdf', '2023-06-24 15:38:00', 3, 7),
+(38, 'asdfasdfasdfasdf', '2023-06-24 15:38:38', 3, 7),
+(39, 'adsfgasdfgasdf', '2023-06-24 15:41:13', 5, 7);
 
 -- --------------------------------------------------------
 
@@ -182,6 +229,14 @@ ALTER TABLE `tickets_priority`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tickets_responses`
+--
+ALTER TABLE `tickets_responses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ticket_id` (`ticket_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `tickets_state`
 --
 ALTER TABLE `tickets_state`
@@ -214,13 +269,19 @@ ALTER TABLE `users_roles`
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tickets_priority`
 --
 ALTER TABLE `tickets_priority`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tickets_responses`
+--
+ALTER TABLE `tickets_responses`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `tickets_state`
@@ -258,6 +319,13 @@ ALTER TABLE `tickets`
   ADD CONSTRAINT `tickets_ibfk_3` FOREIGN KEY (`priority`) REFERENCES `tickets_priority` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `tickets_ibfk_4` FOREIGN KEY (`state`) REFERENCES `tickets_state` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `tickets_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `tickets_responses`
+--
+ALTER TABLE `tickets_responses`
+  ADD CONSTRAINT `tickets_responses_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tickets_responses_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `users`
